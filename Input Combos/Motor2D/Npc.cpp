41ff58138f86f65ee4e1f6f480b8ba5098507c39
BeQuiet <kayter72@gmail.com>
2018-05-13 00:58:23 +0200

    pushbacks

--- Input Combos/Motor2D/Npc.cpp
+++ Input Combos/Motor2D/Npc.cpp
@@ -2,7 +2,8 @@
 
 Npc::Npc(int x, int y, EntityType type) : Entity(x,y,type)
 {
-	anims[NoPass].PushBack({ 0,0,0,0 });
+	anims[NoPass].PushBack({ 5,151,11,48 });
+	anims[NoPass].PushBack({ 20,151,11,48 });
 	anims->speedFactor = 9.0f;
 
 	anim = &anims[NoPass];
