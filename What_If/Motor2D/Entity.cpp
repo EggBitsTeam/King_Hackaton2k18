2f6246078b75dcad0ffef09c01a904aa20e5ed97
BeQuiet <kayter72@gmail.com>
2018-05-12 11:25:10 +0200

    Working on player

--- What_If/Motor2D/Entity.cpp
+++ What_If/Motor2D/Entity.cpp
@@ -1,36 +1,18 @@
 #include "ctApp.h"
 #include "Entity.h"
-#include "ctRender.h"
-#include "ctAudio.h"
-#include "ctLog.h"
+#include "ctEntities.h"
 
-//randomize libs
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include "j1Printer.h"
 
-Entity::Entity(int x, int y, EntityType type) : position(x, y), type(type)
+Entity::Entity(int x, int y, EntityType type) : pos(x, y), type(type)
 {
-
-	/* initialize random seed: */
-	srand(time(NULL));
 }
 
 Entity::~Entity()
 {
-	App->tex->UnLoad(texture);
 }
 
 void Entity::Draw()
 {
-	SDL_Rect r = animation->GetCurrentFrame();
-	
-
-	if (animation != nullptr) {
-		if (flip_texture == false)
-		App->render->Blit(texture, position.x, position.y - r.h, &(animation->GetCurrentFrame()));
-		else {
-		App->render->Blit(texture, position.x, position.y - r.h, &(animation->GetCurrentFrame()), NULL,NULL,255, SDL_FLIP_HORIZONTAL);
-		}
-	}
-
+	App->printer->PrintSprite(pos, (SDL_Texture*)App->entities->GetAtlas(), anim->GetCurrentFrame(), layerToBlit);
 }
--- What_If/Motor2D/ctEntities.cpp
+++ What_If/Motor2D/ctEntities.cpp
@@ -1,13 +1,9 @@
 #include "ctApp.h"
-#include "ctRender.h"
 #include "ctEntities.h"
 #include "ctTextures.h"
 #include "Entity.h"
-#include "ctAudio.h"
-#include "ctWindow.h"
 #include "ctLog.h"
-#include "ctFadeToBlack.h"
-
+#include "Player.h"
 
 ctEntities::ctEntities()
 {
@@ -18,7 +14,6 @@ ctEntities::ctEntities()
 ctEntities::~ctEntities()
 {
 	LOG("Unloading entities spritesheet");
-	
 }
 
 bool ctEntities::Awake(pugi::xml_node& config)
@@ -26,15 +21,13 @@ bool ctEntities::Awake(pugi::xml_node& config)
 	LOG("Loading Entities from config file");
 	bool ret = true;
 
-	//cleric_spritesheet_name = config.child("cleric").attribute("spritesheetName").as_string();
+	atlasEntities = App->tex->Load("");
 	
 	return ret;
 }
 
 bool ctEntities::Start()
 {
-	
-
 	return true;
 }
 
@@ -49,14 +42,6 @@ bool ctEntities::PreUpdate()
 		}
 	}
 
-	if (entities.size() != draw_priority_entities.size())
-	{
-		draw_priority_entities = entities;
-	}
-
-	OrderDrawEntities();
-
-	
 	return true;
 }
 
@@ -66,20 +51,16 @@ bool ctEntities::Update(float dt)
 	for (int i = 0; i < entities.size(); i++)
 		if (entities.at(i) != nullptr) entities[i]->Update(dt);
 
-	for (int i = 0; i <draw_priority_entities.size(); i++)
-		if (draw_priority_entities.at(i) != nullptr) draw_priority_entities[i]->Draw();
-
+	for (int i = 0; i <  entities.size(); i++)
+		if (entities.at(i) != nullptr) entities[i]->Draw();
 
 	return true;
-
 }
 
 // Called before quitting
 bool ctEntities::CleanUp()
 {
 	LOG("Freeing all entities");
-
-
 
 	for (uint i = 0; i < entities.size(); ++i)
 	{
@@ -94,6 +75,7 @@ bool ctEntities::CleanUp()
 
 	entities.clear();
 
+	App->tex->UnLoad(atlasEntities);
 
 	return true;
 }
@@ -102,86 +84,23 @@ bool ctEntities::CleanUp()
 bool ctEntities:: SpawnEntity(int x, int y, EntityType type)
 {
 	// find room for the new entity
-	bool ret = false;
-
-
+	bool ret = true;
 
 	switch (type)
 	{
-	case EntityType::CLERIC: {
-		/*Cleric* cleric = new Cleric(x, y, CLERIC);
-		entities.push_back(cleric);
-		App->combat->turn_priority_entity.push_back(cleric);
-		ret = true;*/
+	case EntityType::PLAYER: {
+		Player* ourPlayer = new Player(x, y, PLAYER);
+		entities.push_back(ourPlayer);
 		break;
 	}
 	default:
 		break;
 	}
 
-
 	return ret;
 }
 
-void ctEntities::OrderDrawEntities()
+const SDL_Texture * ctEntities::GetAtlas() const
 {
-	bool ordered = false;
-	
-	std::vector<Entity*> order_entity = draw_priority_entities;
-
-	while (!ordered)
-	{
-   		ordered = true;
-		std::vector<Entity*>::iterator itnext = order_entity.begin();
-		int count = 0;
-		for (std::vector<Entity*>::iterator it = order_entity.begin(); it != order_entity.end(); ++it)
-		{
-			itnext++;
-			count++;
-			if (count != order_entity.size())
-			{
-				if ((*it)->position.y > (*itnext)->position.y )
-				{
-					Entity* entity_tmp = (*it);
-
-					(*it) = (*itnext);
-					it++;
-					(*it) = entity_tmp;
-					it--;
-					ordered = false;
-
-				}
-				else if ((*it)->position.y == (*itnext)->position.y)
-				{
-
-					if (((*itnext)->type != CLERIC && (*itnext)->type != WARRIOR && (*itnext)->type != ELF && (*itnext)->type != DWARF) && ((*it)->type == CLERIC || (*it)->type == WARRIOR || (*it)->type == ELF || (*it)->type == DWARF))
-					{
-						Entity* entity_tmp = (*it);
-
-						(*it) = (*itnext);
-						it++;
-						(*it) = entity_tmp;
-						it--;
-						ordered = false;
-					}
-				}
-				
-			}
-			else {
-				break;
-			}
-
-		}
-
-	}
-	ordered = false;
-
-
-
-
-
-	draw_priority_entities = order_entity;
-
-	order_entity.clear();
+	return nullptr;
 }
-
